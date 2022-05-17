e3bac35bb393dc671ef39fb793345473d8c54f15
JosephShin <joespam9999@gmail.com>
2022-05-17 15:59:52 -0400

    commit tree 6848c11ae8440bdc0424291cc6147a654351ccd1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    attr_accessor :array
+    def initialize(word)
+        @word = word
+    end
+
+    
+    def match(array)
+        binding.pry
+        i=0
+        sortedWord = ''
+        matchedArray = []
+        while i < array.length
+            sortedWord = array[i].chars.sort.join
+            if sortedWord == word.chars.sort.join
+                matchedArray.push(array[i])
+            end
+            i +=1
+            matchedArray
+        end
+        matchedArray
+    end
+
+end
+
+binding.pry
